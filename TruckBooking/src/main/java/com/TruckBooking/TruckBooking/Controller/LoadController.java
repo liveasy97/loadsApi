f6531d2292d48927de7fc8e7d1e898be4109ed01
7aakansha <7aakansha@gmail.com>
2022-04-13 22:55:43 +0530

    this was comment

--- TruckBooking/src/main/java/com/TruckBooking/TruckBooking/Controller/LoadController.java
+++ TruckBooking/src/main/java/com/TruckBooking/TruckBooking/Controller/LoadController.java
@@ -25,16 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class LoadController {
-// this is
-
-
-
-
-
-
-
-
-	
+/// this is the new change of controller class
 	@Autowired
 	public LoadServiceImpl loadService;
 
